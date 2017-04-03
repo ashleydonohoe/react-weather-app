7ecaea3789de77528767205841688ff31360e6ce
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-02 22:45:42 -0400

    added content to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,13 @@ let React = require("react");
 let About = (props) => {
     return (
         <div>
-            <h3>About</h3>
+            <h1 className="text-center">About</h1>
             <p>This website uses the OpenWeatherMap API to allow you to get the current temperature in any given city.</p>
+            <p>Check out OpenWeatherMap's website and this app's code on GitHub:</p>
+            <div className="button-group">
+                <a target="_blank" href="http://openweathermap.org/" className="button columns small-6">OpenWeatherMap</a>
+                <a target="_blank" href="https://github.com/ashleydonohoe/react-weather-app" className="button columns small-6">App Code</a>
+            </div>
         </div>
     );
 };
