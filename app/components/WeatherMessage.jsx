cf585861b1f661434a8c84078042b86771bbc15a
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-07 17:13:37 -0400

    added weather icons

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,12 +2,36 @@ let React = require("react");
 
 // Destructured from props
 let WeatherMessage = ({temp, wind, humidity, description, location}) => {
-    return (
+
+    function chooseWeatherIcon(description) {
+        let iconName = "";
+        let termToSearch = description.toLowerCase();
+
+        if (termToSearch.includes("rain") || termToSearch.includes("drizzle")) {
+            iconName = "fa fa-5x fa-umbrella";
+        } else if (termToSearch.includes("cloud")) {
+            iconName = "fa fa-5x fa-cloud";
+        } else {
+            // Fallback icon
+            iconName = "fa fa-5x fa-sun-o";
+        }
+
+        return (
+            <p className="text-center">
+                <i className={iconName} aria-hidden="true"></i>
+            </p>
+        );
+    }
+
+     return (
         <div className="row">
             <div className="columns medium-12">
                 <div className="card">
                     <div className="card-divider">
                         <h4 className="text-center">Current Weather <br/> {location}</h4>
+                    </div>
+                    <div class="weather-icon">
+                        { chooseWeatherIcon(description)}
                     </div>
                     <ul className="weather-info">
                         <li><span className="bold">Temperature:</span> {temp} F</li>
